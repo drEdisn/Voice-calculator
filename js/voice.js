bbd54a2e967e02225043e80500f2daf68f050c20
drEdisn <denizs00@mail.ru>
2022-07-07 03:22:40 +0300

    fix: fix bug with multiplication

--- js/voice.js
+++ js/voice.js
@@ -11,15 +11,16 @@ export default class Voice {
     const speech = window.speechSynthesis;
 
     const voice = document.querySelector('.voise__img');
+    const input = document.querySelector('.panel');
 
     voice.onclick = () => recognition.start();
 
     recognition.onresult = (event) => {
       const strArr = event.results[0][0].transcript.split(' ');
-      const str = strArr.map(i => i === 'х' ? i = '*' : i).join(' ')
-      console.log(str);
+      const str = strArr.map(i => i == 'х' || i == 'x' ? i = '*' : i).join(' ');
       const res = Math.trunc(eval(str));
       this.speech.say(String(res).split('').join(' '), speech);
+      input.value = res;
     }
   }
 }
